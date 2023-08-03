c54988b51642b97fdfceae77aaa18770314fe747
umehara <a603751058@gmail.com>
2023-08-03 21:32:29 +0800

    ショップの営業状態モジュール開発　　完成 1.redisTemplateの配置 2.opsForValueの利用

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,27 @@
+package com.sky.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        log.info("redisテンプレートを作り出し始める");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+
+        return redisTemplate;
+
+
+    }
+}
